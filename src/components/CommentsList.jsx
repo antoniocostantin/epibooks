67ca410c122f94cd7bdfced6fcc851a3e97f8188
antoniocostantin <costantini062@gmail.com>
2024-08-22 17:49:31 +0200

    1 extra

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -4,25 +4,6 @@ const key =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmI2OTI4YWI5NjAwMTU2NjRmMGEiLCJpYXQiOjE3MjQzMjg4MDksImV4cCI6MTcyNTUzODQwOX0.wnBh_-sJ2xk_WEUOpVkm7v46qPvgLfKyFzSFbs1YXJg";
 
 class CommentsList extends Component {
-  handleDelete = (id) => {
-    fetch(`https://striveschool-api.herokuapp.com/api/comments/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: key,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("ELIMINAZIONE RIUSCITA");
-        } else {
-          throw new Error("La chiamata non è andata a buon fine");
-        }
-      })
-      .catch((err) => {
-        console.log("ERRORE NEL RECUPERO DATI (internet)?", err);
-      });
-  };
-
   render() {
     return (
       <>
@@ -34,8 +15,29 @@ class CommentsList extends Component {
               {comment.comment} --- {comment.rate}⭐<br />
               <Button
                 type="delete"
-                variant="outline-danger"
-                onClick={this.handleDelete(comment._id)}
+                variant="danger"
+                onClick={(e) => {
+                  e.preventDefault();
+                  fetch(
+                    `https://striveschool-api.herokuapp.com/api/comments/${comment._id}`,
+                    {
+                      method: "DELETE",
+                      headers: {
+                        Authorization: key,
+                      },
+                    }
+                  )
+                    .then((response) => {
+                      if (response.ok) {
+                        alert("ELIMINAZIONE RIUSCITA");
+                      } else {
+                        throw new Error("La chiamata non è andata a buon fine");
+                      }
+                    })
+                    .catch((err) => {
+                      console.log("ERRORE NEL RECUPERO DATI (internet)?", err);
+                    });
+                }}
                 className="m-2"
               >
                 {" "}
